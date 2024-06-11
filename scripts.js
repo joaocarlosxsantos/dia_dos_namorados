f613b6dba11fa509425c694db814ba02ea44b84f
João Carlos <41877895+joaocarlosxsantos@users.noreply.github.com>
2024-06-11 18:37:00 -0300

    alteracoes gerais

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', function() {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
@@ -24,34 +23,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
 document.addEventListener('DOMContentLoaded', function() {
-    // Data alvo em UTC para evitar problemas de fuso horário
-    const targetDate = new Date(Date.UTC(2022, 10, 11, 0, 0, 0)); // Mês 10 porque janeiro é 0
+    const targetDate = new Date(Date.UTC(2022, 10, 11, 0, 0, 0));
 
     function updateCountup() {
-        const currentDate = new Date(); // Data atual
-        const difference = currentDate.getTime() - targetDate.getTime(); // Diferença em milissegundos
+        const currentDate = new Date();
+        const difference = currentDate.getTime() - targetDate.getTime();
 
         if (difference < 0) {
             console.error('A data alvo é no futuro. Verifique a data.');
             return;
         }
 
-        // Calculando o tempo passado
         const days = Math.floor(difference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-        // Atualizando os elementos HTML
         document.getElementById('days').textContent = days.toString().padStart(2, '0');
         document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
         document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
         document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
     }
 
-    // Atualiza o contador a cada segundo
     setInterval(updateCountup, 1000);
-    updateCountup(); // Chama a função imediatamente para evitar o atraso de 1 segundo na inicialização
+    updateCountup();
 });
